eea6aba3d1f3fc814094d22edad314b01be3c606
Alex Seitz <aseitz1307@googlemail.com>
2026-05-17 10:20:43 +0200

    test: StreakManager — Reset bei Fehler/Zeitablauf/Prestige

--- test/test_streak_manager.gd
+++ test/test_streak_manager.gd
@@ -18,3 +18,42 @@ func test_multiplier_caps_at_three() -> void:
 		GameEvents.combo_triggered.emit("row")
 	assert_int(StreakManager.count).is_equal(15)
 	assert_float(StreakManager.multiplier).is_equal_approx(3.0, 0.001)
+
+func test_wrong_cell_resets() -> void:
+	GameEvents.combo_triggered.emit("row")
+	GameEvents.combo_triggered.emit("row")
+	assert_int(StreakManager.count).is_equal(2)
+	GameEvents.cell_filled.emit(false)
+	assert_int(StreakManager.count).is_equal(0)
+	assert_float(StreakManager.multiplier).is_equal_approx(1.0, 0.001)
+
+func test_correct_cell_does_not_reset() -> void:
+	GameEvents.combo_triggered.emit("row")
+	GameEvents.combo_triggered.emit("row")
+	GameEvents.cell_filled.emit(true)
+	assert_int(StreakManager.count).is_equal(2)
+
+func test_time_expires_resets() -> void:
+	GameEvents.combo_triggered.emit("row")
+	StreakManager._tick(16.0)
+	assert_int(StreakManager.count).is_equal(0)
+	assert_float(StreakManager.multiplier).is_equal_approx(1.0, 0.001)
+
+func test_combo_refreshes_window() -> void:
+	GameEvents.combo_triggered.emit("row")
+	StreakManager._tick(10.0)
+	GameEvents.combo_triggered.emit("row")
+	StreakManager._tick(10.0)
+	assert_int(StreakManager.count).is_equal(2)
+
+func test_prestige_resets() -> void:
+	GameEvents.combo_triggered.emit("row")
+	GameEvents.combo_triggered.emit("row")
+	GameEvents.prestiged.emit(5)
+	assert_int(StreakManager.count).is_equal(0)
+	assert_float(StreakManager.multiplier).is_equal_approx(1.0, 0.001)
+
+func test_tick_early_returns_when_count_zero() -> void:
+	StreakManager._tick(100.0)
+	assert_int(StreakManager.count).is_equal(0)
+	assert_float(StreakManager.time_left).is_equal_approx(0.0, 0.001)
